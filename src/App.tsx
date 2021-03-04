0111b73c6dd5a2978cb73d01d5313df684cb341d
Andrew Lloyd <andrew.lloyd01@googlemail.com>
2021-03-04 11:20:31 -0800

    add helvetica-neue font family to styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import './styles/main.css';
 
 interface AppProps {}
 
 const App = () => {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>INTERACTIVE CONCERT EXPERIENCE</h1>
     </div>
   );
 };
